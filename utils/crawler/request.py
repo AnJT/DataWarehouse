b5b2ba316d6e167f1469c58ee25873e4e7291715
potassiummmm <1574996073@qq.com>
2021-11-03 08:45:43 +0800

    Update retry attempt for robot detected situation

--- utils/crawler/request.py
+++ utils/crawler/request.py
@@ -1,5 +1,6 @@
 import time
 import requests
+from bs4 import BeautifulSoup
 
 base_url = 'https://www.amazon.com/dp/'
 review_base_url = 'https://www.amazon.com/product-reviews/'
@@ -15,14 +16,21 @@ def delete_proxy(proxy):
 
 def get_html(asin: str, headers):
     retry_count = 5
-    proxy = get_proxy().get("proxy")
-    while retry_count > 0:
-        try:
-            html = requests.get(url=base_url + asin, headers=headers, proxies={"http": "http://{}".format(proxy)})
-            review_html = requests.get(url=review_base_url + asin, headers=headers, proxies={"http": "http://{}".format(proxy)})
-            return html, review_html
-        except Exception:
-            retry_count -= 1
-            time.sleep(0.5)
-    delete_proxy(proxy)
+    retry_proxy_count = 3
+    while retry_proxy_count > 0:
+        proxy = get_proxy().get("proxy")
+        while retry_count > 0:
+            try:
+                html = requests.get(url=base_url + asin, headers=headers, proxies={"http": "http://{}".format(proxy)})
+                result = html.text.encode('gbk', 'ignore').decode('gbk')
+                soup = BeautifulSoup(result, 'lxml')
+                movie_title = str(soup.select('title')[0].getText())
+                if (movie_title == 'Robot Check') or (movie_title == 'Sorry! Something went wrong!') or (movie_title == 'Amazon.com'):
+                    review_html = requests.get(url=review_base_url + asin, headers=headers, proxies={"http": "http://{}".format(proxy)})
+                    return html, review_html
+            except Exception:
+                retry_count -= 1
+                time.sleep(0.5)
+        delete_proxy(proxy)
+        retry_proxy_count -= 1
     return None, None
